9492a874dd79db59f4239216fcfe4911f9ca3866
Israel Bill <bisrael88@gmail.com>
2022-10-06 14:38:44 +0300

    *ticket

--- screens/Details.js
+++ screens/Details.js
@@ -8,7 +8,7 @@ const DATA = [
     id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
     title: 'Black Pearl Launge and Grill',
     vibe:'Happy Hour',
-    location:"Mamboleo Stage,Off - Vihiga Road, Kisumu",
+    location:"Mamboleo Stage , Off - Vihiga Road , Kisumu",
     banner: 'https://kenyaonthego.com/wp-content/uploads/2021/11/clubda-1-520x397.jpg',
   },
   {
@@ -35,34 +35,49 @@ const DATA = [
 
 
 const Details= ({navigation}) => {
+  const BackButton = () => (
+    <TouchableOpacity style={styles.BackButtonContainer} onPress={() => 
+  
+      navigation.goBack()
+    } >
+      <IconComponentProvider IconComponent={MaterialCommunityIcons}>
+        <Icon name="arrow-left" size={24} color="white"/>
+      </IconComponentProvider>
+    </TouchableOpacity>
+  )
   const Item = ({ title ,banner,location,vibe}) => (
-
-    <View >
+    <View>
     
-     <TouchableOpacity style={styles.item} onPress={() =>
+      <BackButton/>
+    
+    <View style={styles.item}>
+    
+     <TouchableOpacity  onPress={() =>
         navigation.navigate('Venue', { name: 'Jane' })
       } >
      <ImageBackground  
       style={styles.tinyLogo}
       resizeMode="cover"
       source={{ uri: banner }}>
+      <View style={styles.heartIcon}>
+      <IconComponentProvider IconComponent={MaterialCommunityIcons}>
+      <Icon name="heart" size={24} color="white"/>
+      </IconComponentProvider>
+      </View>
   <View style={styles.cardDetails}>
+      
       <View  style={styles.bannerDetails}>
       <Text style={styles.vibe}>{vibe}</Text>
       <Text style={styles.location}>{title}</Text>
       <Text style={styles.road}>{location}
       </Text>
       </View>
-      <View style={styles.heartIcon}>
-      <IconComponentProvider IconComponent={MaterialCommunityIcons}>
-      <Icon name="heart" size={24} color="white"/>
-      </IconComponentProvider>
-      </View>
       </View>
       </ImageBackground>
       <Text style={styles.title}>No reviews yet</Text>
       </TouchableOpacity>
     
+    </View>
     </View>
   );
   
@@ -88,39 +103,59 @@ const styles = StyleSheet.create({
     flex: 1,
     marginTop: StatusBar.currentHeight || 0,
   },
+   
+
   item: {
     flexDirection:'column',
-    backgroundColor: '#2e2e300e',
-    padding: 15,
+    backgroundColor: 'white',
+    
+    padding: 10,
     width:'100%',
     height:380,
     marginVertical: 8,
-    marginTop:30,
+    marginTop:35,
     marginHorizontal: 1,
+    shadowColor: '#000',
+    shadowOffset: {width: -2, height: 4},
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+  },
+  BackButtonContainer:{
+    backgroundColor:"black",
+    opacity:.5,
+    borderRadius:200,
+    alignItems:'center',
+    marginTop:-20,
+    marginBottom:120,
+    marginLeft:10,
+    padding:5,
+    width:50,
   },
   heartIcon: {
   backgroundColor:"rgba(24,24,24,0.4)",
+  position:'absolute',
+  alignItems:'center',
   height:44,
-  width:44,
-  marginTop:190,
-  marginLeft:'10%',
+  width:30,
+  marginTop:15,
+  marginLeft:'70%',
   padding:10,
-  borderRadius:'50%',
+  borderRadius:100,
   },
   cardDetails:{
    flexDirection:'row',
-   alignItems:'flex-start',
+   alignItems:'center',
    justifyContent:'center',
    flex:1,
-   
   },
   tinyLogo: {
-    width:'99%',
-    height:'94%',
-    borderRadius:100,
-    backgroundColor:' #000',
-    opacity: .7,
-    borderRadius:20,
+    width:'100%',
+    height:'100%',
+    backgroundColor:"rgba(24,24,24,0.4)",
+    opacity: .8,
+  
+    borderRadius:200,
+    
 
   },
   title: {
@@ -138,14 +173,16 @@ const styles = StyleSheet.create({
     width:250,
   },
   location:{
-    fontSize: 20,
-    color:'#fff',
-    fontWeight: '600',
+    fontSize: 18,
+    color:'#ffffff',
+    fontWeight: 'bold',
     textAlign:'center',
+   
 
   },
   road:{
-    fontSize: 16,
+    fontSize: 15,
+    marginTop:10,
     color:'#e8e9eb',
     fontWeight: '300',
     textAlign: 'center',
